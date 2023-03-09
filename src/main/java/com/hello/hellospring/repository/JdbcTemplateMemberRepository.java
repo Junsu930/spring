e52ede5f6602dc30222a237c22b58e0eb48eb9ec
junsu <bobdylan0331@gmail.com>
2023-03-10 08:28:05 +0900

    spring jdbc template

---
+++ src/main/java/com/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,35 @@
+package com.hello.hellospring.repository;
+
+import com.hello.hellospring.domain.Member;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate;
+
+    public JdbcTemplateMemberRepository(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
